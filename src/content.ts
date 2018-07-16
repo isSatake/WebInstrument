8d269cc34b62d81300fd2df778a0d264deb8a23b
stkay <stkayy@gmail.com>
2018-07-16 15:18:01 +0900

    スクボで演奏できる

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,60 @@
 import * as WebAudioFontPlayer from "webaudiofont";
 import * as piano from "../lib/sound/0000_Aspirin_sf2_file";
 
-console.log(piano);
 const ctx = new AudioContext();
 const player = new WebAudioFontPlayer();
-
 const tone = piano;
-const when = 0;
-const noteNumber = 60;
-const duration = 1;
-const volume = 1;
-
-setInterval(() => {
-    console.log("play!");
-    player.queueWaveTable(ctx, ctx.destination, tone, when, noteNumber, duration, volume);
-}, 3000);
 
 console.log("Hello from WebInstrumentExtension");
+
+const midiNoteOn = (pitch, velocity) => {
+    console.log("midiNoteOn", pitch, velocity);
+    midiNoteOff(pitch);
+    const envelope = player.queueWaveTable(ctx, ctx.destination, tone, 0, pitch, 123456789, velocity / 100);
+    const note = {
+        pitch: pitch,
+        envelope: envelope
+    };
+};
+
+const midiNoteOff = (pitch) => {};
+
+const onMIDIStateChange = (event) => {
+    const {manufacturer, name, state} = event.port;
+    console.log('midiOnStateChange', event);
+    console.log(`${manufacturer} ${name} ${state}`);
+};
+
+const onMIDImessage = (event) => {
+    const {data} = event;
+    const cmd = data[0] >> 4;
+    const channel = data[0] & 0xf;
+    const type = data[0] & 0xf0;
+    const pitch = data[1];
+    const velocity = data[2];
+    switch (type) {
+        case 144:
+            midiNoteOn(pitch, velocity);
+            break;
+        case 128:
+            midiNoteOff(pitch);
+            break;
+    }
+};
+
+//MIDIリクエスト
+const requestMIDIAccessFailure = (e) => {
+    console.log('failed to requestMIDIAccess', e);
+};
+const requestMIDIAccessSuccess = (midi) => {
+    console.log("succeeded to requestMIDIAccess");
+    const inputs = midi.inputs.values();
+    for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+        console.log('midi input', input);
+        input.value.onmidimessage = onMIDImessage;
+    }
+    midi.onstatechange = onMIDIStateChange;
+};
+
+console.log("requestMIDIaccess");
+navigator.requestMIDIAccess().then(requestMIDIAccessSuccess, requestMIDIAccessFailure);
