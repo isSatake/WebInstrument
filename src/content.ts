92ad146a899f5ffa4f0983a8fd9b724be006d267
stkay <stkayy@gmail.com>
2018-08-02 02:54:30 +0900

    gyaon対応

--- src/content.ts
+++ src/content.ts
@@ -1,25 +1,39 @@
-import * as WebAudioFontPlayer from "webaudiofont";
-import {ins} from "./instrument";
+import {WebAudioFontPlayer} from "./WebAudioFontPlayer";
+import axios from "axios";
 
 const ctx = new AudioContext();
-const player = new WebAudioFontPlayer();
+const player = WebAudioFontPlayer();
+const instruments = [];
+const midiNotes = [];
 const getPageTitle = () => decodeURI(location.pathname.split("/")[2]);
 let oldPageTitle: string;
-let tone = ins[getPageTitle()];
+let tone = instruments[getPageTitle()];
 
 console.log("Hello from WebInstrumentExtension");
 
 const midiNoteOn = (pitch, velocity) => {
     console.log("midiNoteOn", pitch, velocity);
+    console.log("tone", tone);
     midiNoteOff(pitch);
     const envelope = player.queueWaveTable(ctx, ctx.destination, tone, 0, pitch, 123456789, velocity / 100);
     const note = {
         pitch: pitch,
         envelope: envelope
     };
+    midiNotes.push(note);
 };
 
-const midiNoteOff = (pitch) => {};
+const midiNoteOff = (pitch) => {
+    for (let i = 0; i < midiNotes.length; i++) {
+        if (midiNotes[i].pitch == pitch) {
+            if (midiNotes[i].envelope) {
+                midiNotes[i].envelope.cancel();
+            }
+            midiNotes.splice(i, 1);
+            return;
+        }
+    }
+};
 
 const onMIDIStateChange = (event) => {
     const {manufacturer, name, state} = event.port;
@@ -59,14 +73,87 @@ const requestMIDIAccessSuccess = (midi) => {
     midi.onstatechange = onMIDIStateChange;
 };
 
+const getSoundfontURL = async (pageTitle: string) => {
+    const res = await fetch(`https://scrapbox.io/api/pages/scrapbox-instrument/${pageTitle}`); //axios
+    const {lines} = await res.json();
+    for (let line of lines) {
+        const matched = line.text.match(/https\:\/\/stkay.github.io\/webaudiofontdata\/sound\/.*\.json/);
+        if (matched) {
+            return matched[0]
+        }
+    }
+    for (let line of lines) {
+        const matched = line.text.match(/http.*\.(wav|mp3)/);
+        if (matched) {
+            return matched[0]
+        }
+    }
+    return ""
+};
+
+const getTone = async (url: string): Promise<object> => {
+    if (url.match(/http.*\.(wav|mp3)/)) {
+        return soundfontFromSoundURL(await getDataUrlFromSoundURL(url));
+    }
+    const res = await fetch(url); //axios
+    return await res.json();
+};
+
+const getDataUrlFromSoundURL = (url: string): Promise<string> => {
+    return new Promise(async (resolve, reject) => {
+        const res = await axios.get(url, {responseType: "blob"});
+        console.log(res);
+        const reader = new FileReader();
+        reader.readAsDataURL(res.data);
+        reader.onload = () => {
+            console.log("onload");
+            resolve(reader.result);
+        };
+        reader.onerror = (err) => {
+            reject(err);
+        }
+    });
+};
+
+const soundfontFromSoundURL = (dataUrl: string) => {
+    return {
+        zones: [
+            {
+                midi: 0,						//MIDI program
+                originalPitch: 6100,		    //音声データのピッチ(cent)
+                keyRangeLow: 0,					//低い方のノートナンバー
+                keyRangeHigh: 108,				//高い方
+                loopStart: -1,					//ループ開始sec
+                loopEnd: -1,					//終了sec
+                coarseTune: 1,					//use fine tune
+                fineTune: 0,					//tune correction in cents
+                sampleRate: 44100,				//音声のサンプルレート ループでしか使ってないようなので適当でいい
+                ahdsr: false,					//adsrをいじるかどうか
+                delay: 0,                    //再生開始時間 とりあえずペンディング
+                file: dataUrl.split("data:audio/wav;base64,")[1]    //base64 APIで取れる？
+            }
+        ]
+    }
+};
+
 //ページ遷移をハンドル
-setInterval(() => {
+setInterval(async () => {
     const pageTitle = getPageTitle();
-    if (oldPageTitle !== pageTitle) {
-        console.log("requestMIDIaccess");
-        navigator.requestMIDIAccess().then(requestMIDIAccessSuccess, requestMIDIAccessFailure);
-        tone = ins[pageTitle];
+    if (pageTitle && oldPageTitle !== pageTitle) {
+        console.log("change instrument");
+        if (!instruments[pageTitle]) {
+            //wavなら
+
+            instruments[pageTitle] = await getTone(await getSoundfontURL(pageTitle));
+        }
+        tone = instruments[pageTitle];
         oldPageTitle = pageTitle;
     }
-    midiNoteOn(60, 100);
 }, 1000);
+
+console.log("requestMIDIaccess");
+navigator.requestMIDIAccess().then(requestMIDIAccessSuccess, requestMIDIAccessFailure);
+// (async () => {
+//     console.log("hi");
+//     console.log(await dataUrlFromSoundURL("https://gyaon.com/sound/c6f7d4648a6120c0943e38c2edac4108.wav"));
+// })();
--- src/instrument/index.ts
+++
@@ -1,9 +0,0 @@
-import * as piano from "../../lib/sound/0000_Aspirin_sf2_file";
-import * as harpsichord from "../../lib/sound/0060_SBLive_sf2";
-import * as nylonguitar from "../../lib/sound/0241_JCLive_sf2_file";
-
-export const ins = {
-    piano: piano,
-    harpsichord: harpsichord,
-    nylonguitar: nylonguitar
-};
